91bde2734d9da99f0a2717fb4171166b1277188d
manas1820 <manasgupta1820@gmail.com>
2022-08-30 23:18:09 +0530

    fix : admin intialization

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,8 @@ case "$1" in
   server|"")
     # Start scheduler and webserver in same container
     header "RUNNING MIGRATIONS AND STARTING WSGI SERVER"
-    python manage.py collectstatic
+    python manage.py collectstatic --force-color
+    python manage.py initadmin
     python manage.py migrate
     gunicorn --bind :8000 --workers 3 backend.wsgi
     ;;
